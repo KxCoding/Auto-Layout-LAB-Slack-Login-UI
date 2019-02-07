e892a7da3c8295eb9207147647141a0fd7260a10
Keun young Kim <kky0317@gmail.com>
2019-02-07 19:41:34 +0900

    Step 12

--- KxSlackLogin/EmailViewController.swift
+++ KxSlackLogin/EmailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class EmailViewController: UIViewController {
-
+   
    @IBOutlet weak var titleLabelBottomConstraint: NSLayoutConstraint!
    
    @IBOutlet weak var titleLabel: UILabel!
@@ -24,20 +24,46 @@ class EmailViewController: UIViewController {
       navigationController?.popViewController(animated: true)
    }
    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
+      super.viewDidLoad()
+      
+      titleLabel.alpha = 0.0
+      titleLabelBottomConstraint.constant = -20
+   }
+   
+   
+   /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
     }
     */
+   
+}
 
+
+extension EmailViewController: UITextFieldDelegate {
+   func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+      placeholderLabel.alpha = (textField.text ?? "").count > 0 ? 0.0 : 1.0
+      
+      return true
+   }
+   
+   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+      let finalText = NSMutableString(string: textField.text ?? "")
+      finalText.replaceCharacters(in: range, with: string)
+      
+      placeholderLabel.alpha = finalText.length > 0 ? 0.0 : 1.0
+      
+      UIView.animate(withDuration: 0.3) { [weak self] in
+         self?.titleLabel.alpha = finalText.length > 0 ? 1.0 : 0.0
+         self?.titleLabelBottomConstraint.constant = finalText.length > 0 ? 0 : -20
+         
+         self?.view.layoutIfNeeded()
+      }
+      
+      return true
+   }
 }
